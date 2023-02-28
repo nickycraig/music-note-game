2faf268b668e6cd59f83502ffef6208f598bd95e
nickycraig <nickycraig@gmail.com>
2023-02-27 16:14:28 -0800

    changed html/css and added some JS

---
+++ musicnotes.js
@@ -0,0 +1,10 @@
+const roundNumber = document.querySelector('.round-number');
+const nextButton = document.querySelector('.next');
+
+const buttonA = document.querySelector('.a');
+const buttonB = document.querySelector('.b');
+const buttonC = document.querySelector('.c');
+const buttonD = document.querySelector('.d');
+const buttonE = document.querySelector('.e');
+const buttonF = document.querySelector('.f');
+const buttonG = document.querySelector('.g');
